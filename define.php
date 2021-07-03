5035b8a5c803624bde126a09af4af20b3ea20439
unknown <tyagiayushi9953@gmail.com>
2021-07-03 20:44:30 +0530

    add snapdeal

---
+++ define.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>camera</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Sofia&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/all.min.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!---section-one--->
+<section class="section-one_a">
+	<div class="my-container_a">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="top-header_a">India's Fastest Online Shopping Destination</div>
+			</div>
+			<div class="col-md-6">
+				<div class="top-header_a">
+					<ul>
+						<li><a href="#">Gift Cards</a></li>
+						<li><a href="#">Help Center</a></li>
+						<li><a href="#">Sell On Snapdeal</a></li>
+						<li><a href="#"><span class="top-header-icon_a"><i class="fas fa-file-download"></i></span>Download App</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!---section-two--->
+<section class="section-two_a">
+	<div class="my-container_a">
+		<nav class="navbar navbar-expand-sm  navbar-dark">
+		<div class="navbar-brand" href="index.php">
+			<a href="index.php"><img src="asset/snapfull.png" alt="Logo" style="width:100%; max-width:168px;"></a>
+			<i class="fas fa-bars"></i>
+		</div>
+		<form class="form-inline" action="/action_page.php" style="flex:1">
+			<div class="header-search_a">
+				<input class="form-control mr-sm-2" type="text" placeholder="Search products & brands">
+				<button><i class="fas fa-search"></i>Search</button>
+			</div>
+		</form>
+		<div class="header-ul_a">
+			<ul class="nav justify-content-end ">
+				<li class="nav-item">
+					<a class="nav-link cart_a" href="#"><span>Cart</span><i class="fas fa-shopping-cart"></i></a>
+				</li>
+				<li class="nav-item sign-in_a">
+					<a class="nav-link" href="#"><span>Sign In</span><i class="fas fa-user"></i></a>
+				</li>
+			</ul>
+		</div>
+		</nav>
+	</div>
+</section>
+
+<!---section-three--->
+<section class="section-three_d">
+	<div class="my-container_a">
+		<div class="main-div-three_d">
+			<nav class="navbar navbar-expand-sm bg-light">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Home</a>
+					</li>
+					<span>/</span>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Kitchenware</a>
+					</li>
+					<span>/</span>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Kitchen Tools</a>
+					</li>
+					<span>/</span>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Graters</a>
+					</li>
+					<span>/</span>
+					<span>Mini Juicer Machine.Juice Maker Machine for Home.Deluxe Fruit & Vegetable Manual Juicer with with Steel Handel</span>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</section>
+
+<?php include 'includes/fotter.php'; ?>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
