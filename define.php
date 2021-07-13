7ca578a4e547b5e5d10198392e9454cfcc84e939
Kapil Chauhan <kapil.chauhan@mamsys.com>
2021-07-13 21:04:17 +0530

    update project

--- define.php
+++ define.php
@@ -141,6 +141,8 @@ $rowprodlis = mysqli_fetch_array($queryprodlis);
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+  
+  
 
 </script>
 </html>
